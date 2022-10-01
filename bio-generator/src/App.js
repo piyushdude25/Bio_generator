0115b04b651558db8ce8c318e4781612ba20f415
piyushdude25 <piyushdude25@gmail.com>
2022-10-01 20:42:44 +0530

    add-redux

--- bio-generator/src/App.js
+++ bio-generator/src/App.js
@@ -1,10 +1,44 @@
-import "./App.css";
-import Body from "./components/Body";
+import React from "react";
+
+// import "./static/scss/app.scss";
+import "react-router-dom";
+import { Route, Switch, Routes } from "react-router-dom";
+// import Header from "./components/presentation/header";
+// import Footer from "./components/presentation/footer";
+import LandingPage from "./components/presentation/LandingPage";
+import GettingStarted from "./components/presentation/gettingStarted";
+import Login from "./components/presentation/login";
+import Register from "./components/presentation/register";
+// import AboutUs from "./components/presentation/aboutUs";
+import Contacts from "./components/presentation/contact";
+import Education from "./components/presentation/education";
+import Finalize from "./components/presentation/finalizePage";
+// import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <Body />
+    <div>
+      {/* <Header></Header> */}
+
+      {/* <Switch> */}
+      {/* <Routes> */}
+      {/* <PrivateRoute path="/education" component={Education}></PrivateRoute> */}
+      {/* <PrivateRoute path="/contact" component={Contacts}></PrivateRoute> */}
+      {/* <Route path="/getting-started" component={GettingStarted}></Route> */}
+      {/* <PrivateRoute
+          path="/resume-templates"
+          component={GettingStarted}
+        ></PrivateRoute> */}
+      {/* <Route path="/about-us" component={AboutUs}></Route> */}
+      {/* <PrivateRoute path="/finalize" component={Finalize}></PrivateRoute> */}
+      {/* <Route path="/login" component={Login}></Route>
+      <Route path="/register" component={Register}></Route>
+      <Route path="/" component={LandingPage}></Route> */}
+      {/* </Switch> */}
+      {/* </Routes> */}
+      {/* <Footer></Footer> */}
+      <Education />
+      <GettingStarted />
     </div>
   );
 }
---
+++ bio-generator/src/redux/actions/documentActions.js
@@ -0,0 +1,21 @@
+import * as documentActions from "./actions";
+import { v4 as uuidv4 } from "uuid";
+
+export const setSkin = (skinCd) => {
+  return {
+    type: documentActions.SET_SKIN,
+    payload: {
+      skinCd: skinCd,
+      id: uuidv4(),
+    },
+  };
+};
+
+export const updateSkin = (skinCd) => {
+  return {
+    type: documentActions.UPDATE_SKIN,
+    payload: {
+      skinCd: skinCd,
+    },
+  };
+};
